bba030d879bf4868b5bbd523c388cf3a8632fb47
Rin Nguyen <sonrin746@gmail.com>
2022-12-18 07:18:58 +0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../demo-styling.css']
+  //styleUrls: ['../demo-styling.css']
 })
 export class AppComponent {
   title = 'angular-quickstart';
