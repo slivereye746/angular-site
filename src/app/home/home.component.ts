cb25fbb529797ca6900da4f0f0d5dbe4e9084260
Rin Nguyen <sonrin746@gmail.com>
2023-05-24 19:52:57 +0700

    x

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,32 +6,10 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  groups = [
-    { items: ['Item 1', 'Item 2', 'Item 3', 'Item 4'] },
-    { items: ['Item 5', 'Item 6', 'Item 7', 'Item 8'] },
-    { items: ['Item 9', 'Item 10', 'Item 11', 'Item 12'] }
-  ];
-  activeGroup = this.groups[0];
-
-  @ViewChild('scrollContainer') scrollContainerRef!: ElementRef;
-
-  scrollToGroup(index: number) {
-    const scrollContainer = this.scrollContainerRef.nativeElement;
-    const groupWidth = scrollContainer.offsetWidth;
-    scrollContainer.scrollLeft = index * groupWidth;
-  }
-
-  updateActiveGroup() {
-    const scrollContainer = this.scrollContainerRef.nativeElement;
-    const scrollLeft = scrollContainer.scrollLeft;
-    const groupWidth = scrollContainer.offsetWidth;
-    const activeIndex = Math.floor(scrollLeft / groupWidth);
-    this.activeGroup = this.groups[activeIndex];
-  }
+  
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
+
