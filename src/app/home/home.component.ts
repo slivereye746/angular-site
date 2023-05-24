82949152edf541f8d013643967118a14ad751b76
Rin Nguyen <sonrin746@gmail.com>
2023-05-24 14:27:34 +0700

    x

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  groups = [
+    { items: ['Item 1', 'Item 2', 'Item 3', 'Item 4'] },
+    { items: ['Item 5', 'Item 6', 'Item 7', 'Item 8'] },
+    { items: ['Item 9', 'Item 10', 'Item 11', 'Item 12'] }
+  ];
+  activeGroup = this.groups[0];
+
+  @ViewChild('scrollContainer') scrollContainerRef!: ElementRef;
+
+  scrollToGroup(index: number) {
+    const scrollContainer = this.scrollContainerRef.nativeElement;
+    const groupWidth = scrollContainer.offsetWidth;
+    scrollContainer.scrollLeft = index * groupWidth;
+  }
+
+  updateActiveGroup() {
+    const scrollContainer = this.scrollContainerRef.nativeElement;
+    const scrollLeft = scrollContainer.scrollLeft;
+    const groupWidth = scrollContainer.offsetWidth;
+    const activeIndex = Math.floor(scrollLeft / groupWidth);
+    this.activeGroup = this.groups[activeIndex];
+  }
   constructor() { }
 
   ngOnInit(): void {
--- src/app/scroll-with-mouse.directive.ts
+++ src/app/scroll-with-mouse.directive.ts
@@ -1,15 +1,37 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appScrollWithMouse]'
 })
 export class ScrollWithMouseDirective {
 
-  @HostListener('wheel', ['$event'])
-  onWheel(event: WheelEvent) {
+  private isMouseDown = false;
+  private startX!: number;
+  private scrollX!: number;
+  private sensitivity = 1.2; // Độ nhạy, có thể điều chỉnh giá trị này
+
+  constructor(private elementRef: ElementRef) { }
+
+  @HostListener('mousedown', ['$event'])
+  onMouseDown(event: MouseEvent) {
+    this.isMouseDown = true;
+    this.startX = event.pageX - this.elementRef.nativeElement.offsetLeft;
+    this.scrollX = this.elementRef.nativeElement.scrollLeft;
+  }
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    if (!this.isMouseDown) {
+      return;
+    }
     event.preventDefault();
-    const element = event.target as HTMLElement;
-    const delta = Math.max(-1, Math.min(1, -event.deltaY));
-    element.scrollLeft -= (delta * 40);
+    const x = event.pageX - this.elementRef.nativeElement.offsetLeft;
+    const moveX = (x - this.startX) * this.sensitivity; // Tăng độ nhạy ở đây
+    this.elementRef.nativeElement.scrollLeft = this.scrollX - moveX;
+  }
+
+  @HostListener('mouseup')
+  onMouseUp() {
+    this.isMouseDown = false;
   }
 }
