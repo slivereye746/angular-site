9240812828e09d1bb42d95d23454452471a7f568
Rin Nguyen <sonrin746@gmail.com>
2023-06-04 14:46:11 +0700

    fix sticky

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,11 +6,4 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  isSticky: boolean = false;
-
-  @HostListener('window:scroll', ['$event'])
-  checkScroll() {
-    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    this.isSticky = scrollPosition >= 1;
-  }
 }
